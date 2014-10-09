e8fe1bfed4fe17ea0e85650861456c70ce0b8f0f
Jeremy Perret <jeremy@devster.org>
2014-10-09 13:51:43 +0200

    rewrite the plugin for guzzle 4

---
+++ src/WsseAuth.php
@@ -0,0 +1,251 @@
+<?php
+
+namespace Devster\GuzzleHttp\Subscriber;
+
+use GuzzleHttp\Message\RequestInterface;
+use GuzzleHttp\Event\EmitterInterface;
+use GuzzleHttp\Event\SubscriberInterface;
+use GuzzleHttp\Event\BeforeEvent;
+use GuzzleHttp\Event\RequestEvents;
+Use GuzzleHttp\Client;
+
+/**
+ * WSSE signing plugin
+ * @link http://www.xml.com/pub/a/2003/12/17/dive.html
+ */
+class WsseAuth implements SubscriberInterface
+{
+    /**
+     * @var string
+     */
+    protected $username;
+
+    /**
+     * @var string
+     */
+    protected $password;
+
+    /**
+     * Date format - PHP style
+     *
+     * Default: ISO8601
+     *
+     * @var string
+     */
+    protected $dateFormat = 'c';
+
+    /**
+     * @var Callable
+     */
+    protected $nonce;
+
+    /**
+     * @var Callable
+     */
+    protected $digest;
+
+    /**
+     * @param string $username
+     * @param string $password
+     */
+    public function __construct($username, $password)
+    {
+        $this->username = $username;
+        $this->password = $password;
+        $this->nonce    = array($this, 'nonce');
+        $this->digest   = array($this, 'digest');
+    }
+
+    /**
+     * Attach the plugin to the client
+     *
+     * @param Client $client
+     *
+     * @return WsseAuth The current instance
+     */
+    public function attach(Client $client)
+    {
+        $client->getEmitter()->attach($this);
+
+        return $this;
+    }
+
+    /**
+     * Set dateFormat
+     *
+     * @param string $dateFormat
+     *
+     * @return WssePlugin The current instance
+     */
+    public function setDateFormat($dateFormat)
+    {
+        $this->dateFormat = $dateFormat;
+
+        return $this;
+    }
+
+    /**
+     * Get dateFormat
+     *
+     * @return string
+     */
+    public function getDateFormat()
+    {
+        return $this->dateFormat;
+    }
+
+    /**
+     * Set nonce
+     *
+     * @param callable $nonce
+     *
+     * @return WssePlugin The current instance
+     */
+    public function setNonce(callable $nonce)
+    {
+        $this->nonce = $nonce;
+
+        return $this;
+    }
+
+    /**
+     * Get nonce
+     *
+     * @return callable
+     */
+    public function getNonce()
+    {
+        return $this->nonce;
+    }
+
+    /**
+     * Set digest
+     *
+     * @param callable $digest
+     *
+     * @return WssePlugin The current instance
+     */
+    public function setDigest(callable $digest)
+    {
+        $this->digest = $digest;
+
+        return $this;
+    }
+
+    /**
+     * Get digest
+     *
+     * @return callable
+     */
+    public function getDigest()
+    {
+        return $this->digest;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getEvents()
+    {
+        return array(
+            'before' => array('onBefore', RequestEvents::SIGN_REQUEST)
+        );
+    }
+
+    /**
+     * On before event handler
+     *
+     * @return array
+     * @throws \InvalidArgumentException
+     */
+    public function onBefore(BeforeEvent $event)
+    {
+        // // Nonce validation
+        // if (! is_callable($this->config['nonce_callback'])) {
+        //     throw new \InvalidArgumentException('Option nonce_callback must be a callable or a Closure');
+        // }
+
+        // $nonce = call_user_func($this->config['nonce_callback'], $event);
+
+        // if (! is_string($nonce) || '' == $nonce) {
+        //     throw new \InvalidArgumentException('Option nonce_callback must return an non empty string');
+        // }
+
+        // // Timestamp validation
+        // if (! is_callable($this->config['timestamp_callback'])) {
+        //     throw new \InvalidArgumentException('Option timestamp_callback must be a callable or a Closure');
+        // }
+
+        // $timestamp = call_user_func($this->config['timestamp_callback'], $event);
+
+        // if (! $timestamp instanceof \DateTime) {
+        //     throw new \InvalidArgumentException('Option timestamp_callback must return a \DateTime instance');
+        // }
+
+        $now       = new \DateTime;
+        $createdAt = $now->format($this->getDateFormat());
+        $request   = $event->getRequest();
+        $nonce     = call_user_func($this->getNonce(), $request);
+        $digest    = call_user_func($this->getDigest(), $nonce, $createdAt, $this->password);
+
+        $request->setHeader('Authorization', 'WSSE profile="UsernameToken"');
+        $request->setHeader(
+            'X-WSSE',
+            $this->createWsseHeader(
+                $this->username,
+                $digest,
+                $nonce,
+                $createdAt
+            )
+        );
+    }
+
+    /**
+     * Create the WSSE header
+     *
+     * @param string $username
+     * @param string $nonce
+     * @param string $createdAt,
+     * @param string $password
+     *
+     * @return string
+     */
+    public function createWsseHeader($username, $digest, $nonce, $createdAt)
+    {
+        return sprintf(
+            'UsernameToken Username="%s", PasswordDigest="%s", Nonce="%s", Created="%s"',
+            $username,
+            $digest,
+            $nonce,
+            $createdAt
+        );
+    }
+
+    /**
+     * Create a password digest
+     *
+     * @param string $nonce
+     * @param string $createdAt
+     * @param string $password
+     *
+     * @return string
+     */
+    public static function digest($nonce, $createdAt, $password)
+    {
+        return base64_encode(sha1($nonce.$createdAt.$password, true));
+    }
+
+    /**
+     * Generate a nonce
+     *
+     * @param RequestInterface $request
+     *
+     * @return string
+     */
+    public static function nonce(RequestInterface $request = null)
+    {
+        $url = $request ? $request->getUrl() : null;
+
+        return sha1(uniqid('', true) . $url);
+    }
+}
---
+++ tests/WsseAuthTest.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace Devster\Test\GuzzleHttp\Subscriber;
+
+use Devster\GuzzleHttp\Subscriber\WsseAuth;
+use GuzzleHttp\Client;
+use GuzzleHttp\Message\Response;
+
+/**
+ * @covers Devster\GuzzleHttp\Subscriber\WsseAuth
+ */
+class WsseAuthTest extends \PHPUnit_Framework_TestCase
+{
+    protected function createPlugin()
+    {
+        return new WsseAuth('John', 'pass');
+    }
+
+    protected function createClient()
+    {
+        return new Client();
+    }
+
+    public function testNonce()
+    {
+        $request = $this->createClient()->createRequest('GET', 'http://example.com');
+
+        $nonces = array();
+
+        for ($i = 0; $i < 10000; $i++) {
+            $nonces[] = WsseAuth::nonce($request);
+        }
+
+        $this->assertEquals(10000, count(array_unique($nonces)));
+    }
+
+    public function testDigest()
+    {
+        $this->assertEquals(
+            base64_encode(sha1('nonce'.'timestamp'.'password', true)),
+            WsseAuth::digest('nonce', 'timestamp','password')
+        );
+    }
+
+    protected function wsseHeaderTest($header, array $expected)
+    {
+        $this->assertNotNull($header);
+        $this->assertEquals(1, preg_match(
+                '/UsernameToken Username="([^"]+)", PasswordDigest="([^"]+)", Nonce="([^"]+)", Created="([^"]+)"/',
+                $header,
+                $matches
+            )
+        );
+
+        foreach ($expected as $key => $val) {
+            $this->assertEquals($val, $matches[$key+1]);
+        }
+    }
+
+    public function testCreateWsseHeader()
+    {
+        $p = $this->createPlugin();
+
+        $header = $p->createWsseHeader('John', 'digest', 'nonce', 'createdAt');
+
+        $this->wsseHeaderTest($header, array(
+            'John',
+            'digest',
+            'nonce',
+            'createdAt'
+        ));
+    }
+
+    public function testFunctional()
+    {
+        $client = $this->createClient();
+        $plugin = $this->createPlugin();
+        $client->getEmitter()->attach($plugin);
+
+        // Prevent the sending
+        $client->getEmitter()->on('before', function ($e) {
+            $response = new Response(200);
+            $e->intercept($response);
+        }, 'last');
+
+        $request = $client->createRequest('GET', 'http://example.com');
+
+        $this->assertEmpty($request->getHeader('Authorization'));
+        $this->assertEmpty($request->getHeader('X-WSSE'));
+
+        $client->send($request);
+
+        // test the Authorization header
+        $this->assertNotNull($header = $request->getHeader('Authorization'));
+        $this->assertEquals($header, 'WSSE profile="UsernameToken"');
+
+        // test the x-wsse header
+        $this->assertNotNull($header = $request->getHeader('X-WSSE'));
+        $this->assertEquals(1, preg_match(
+                '/UsernameToken Username="([^"]+)", PasswordDigest="([^"]+)", Nonce="([^"]+)", Created="([^"]+)"/',
+                $header,
+                $matches
+            )
+        );
+        $this->assertEquals('John', $matches[1]);
+        $this->assertEquals(WsseAuth::digest($matches[3], $matches[4], 'pass'), $matches[2]);
+        $this->assertInternalType('string', $matches[3]);
+        $this->assertRegExp('/(\d{4})-(\d{2})-(\d{2})T(\d{2})\:(\d{2})\:(\d{2})[+-](\d{2})\:(\d{2})/', $matches[4]);
+    }
+
+    public function testCustom()
+    {
+        $client = $this->createClient();
+        $plugin = $this->createPlugin();
+        $plugin
+            ->attach($client)
+            ->setDigest(function () {
+                return 'custom_digest';
+            })
+            ->setNonce(function () {
+                return 'custom_nonce';
+            })
+            ->setDateFormat('m-Y')
+        ;
+        $request = $client->createRequest('GET', 'http://example.com');
+        $client->send($request);
+
+        $this->wsseHeaderTest($request->getHeader('X-WSSE'), array(
+            'John',
+            'custom_digest',
+            'custom_nonce',
+            date('m-Y')
+        ));
+    }
+}
